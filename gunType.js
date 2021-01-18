ac8e2253287468571d3a190e41d6f7d664d85193
Catherine Strassburg <catherinestrassburg@CatherineStrassburgMacBook.hsd1.ma.comcast.net>
2021-01-18 13:03:01 -0500

    Added primaryKey: true to gunType id

--- gunType.js
+++ gunType.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
 module.exports = (sequelize) => {
   const gunType = sequelize.define('gunType', {
-    id: { type: Sequelize.INTEGER, allowNull: false },
+    id: { type: Sequelize.STRING(64), primaryKey: true, allowNull: false },
     name: { type: Sequelize.STRING(64), allowNull: false },
     version: { type: Sequelize.INTEGER, allowNull: false },
-    shipTypeId: { type: Sequelize.INTEGER, allowNull: false },
+    shipTypeId: { type: Sequelize.STRING(64), allowNull: false },
     structure: { type: Sequelize.STRING(128), allowNull: false },
   });
   return gunType;
