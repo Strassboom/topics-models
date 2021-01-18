f44a8d394d17952c8ae7ec964c3d90bab340a2af
Strassboom <maddog241996@gmail.com>
2021-01-18 16:39:41 -0500

    Added positionX and positionY to model

--- sessionEvent.js
+++ sessionEvent.js
@@ -4,6 +4,8 @@ module.exports = (sequelize) => {
     id: { type: Sequelize.STRING(64), primaryKey: true, allowNull: false },
     sessionId: { type: Sequelize.STRING(64), allowNull: false },
     dateTime: { type: Sequelize.DATE, allowNull: false },
+    positionX: { type: Sequelize.FLOAT, allowNull: false },
+    positionY: { type: Sequelize.FLOAT, allowNull: false },
     isFiring: { type: Sequelize.BOOLEAN, allowNull: false },
     health_points: { type: Sequelize.INTEGER, allowNull: false }
   });
